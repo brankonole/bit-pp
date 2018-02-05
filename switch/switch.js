2d957794fa50ec2c9b74e696e297aa19c7044175
BIT student <classroom@bgit.rs>
2018-02-05 16:42:41 +0100

    Switch exercises

---
+++ switch/switch.js
@@ -0,0 +1,88 @@
+// var day = 16;
+
+// switch (day) {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log("It's weekday");
+//         break
+//     case 6:
+//     case 7:
+//         console.log("It's weekend");
+//         break
+//     default:
+//         console.log("Input must be number between 1 and 7");
+//         break      
+// }
+
+// var month = 12;
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break
+//     case 2:
+//         console.log("February");
+//         break
+//     case 3:
+//         console.log("March");
+//         break
+//     case 4:
+//         console.log("April");
+//         break
+//     case 5:
+//         console.log("May");
+//         break
+//     case 6:
+//         console.log("June");
+//         break
+//     case 7:
+//         console.log("July");
+//         break
+//     case 8:
+//         console.log("August");
+//         break
+//     case 9:
+//         console.log("September");
+//         break
+//     case 10:
+//         console.log("October");
+//         break
+//     case 11:
+//         console.log("November");
+//         break
+//     case 12:
+//         console.log("December");
+//         break
+//     default:
+//         console.log("Input must be a number between 1 and 12");
+// }
+
+var month = 4;
+
+switch (month) {
+    case 12:
+    case 1:
+    case 2:
+        console.log("Zima");
+        break
+    case 3:
+    case 4:
+    case 5:
+        console.log("Prolece");
+        break
+    case 6:
+    case 7:
+    case 8:
+        console.log("Leto");
+        break
+    case 9:
+    case 10:
+    case 11:
+       console.log("Jesen");
+       break
+    default:
+        console.log("Input must be a number between 1 and 12"); 
+}
