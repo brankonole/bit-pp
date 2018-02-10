1369d2a715cb35c6d17e20770d37a17c3e5311b7
Branko Novakovic <brankon89@gmail.com>
2018-02-10 14:16:22 +0100

    Function colorReader

---
+++ branch-test/main.js
@@ -0,0 +1,14 @@
+function colorReader(a) {
+    var firstTwo = [a[1] + a[2]],
+        secondTwo = [a[3] + a[4]],
+        thirdTwo = [a[5] + a[6]];
+    firstNum = parseInt(firstTwo ,16);
+    secondNum = parseInt(secondTwo ,16);
+    thirdNum = parseInt(thirdTwo ,16);
+    // console.log(t);
+    finalColor = 'rgb('+ firstNum +', '+ secondNum +', '+ thirdNum +')';
+   
+    return finalColor;
+}
+
+console.log(colorReader("#F3A811"));
