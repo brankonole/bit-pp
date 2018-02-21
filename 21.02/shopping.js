17ccf0049b1c4ccede8222ff3cab574fe2d2676a
Branko Novakovic <brankon89@gmail.com>
2018-02-22 00:14:28 +0100

    Refactoring

--- 21.02/shopping.js
+++ 21.02/shopping.js
@@ -1,52 +1,53 @@
 "use strict";
 
 (function() {
-    console.log('Hi! We can do it!');
-
     function Product(name, price, expirationDate) {
 
         this.id = (function() {
-            var productId;
-            // magic
-            productId = 13521;
+            var productId = Math.floor(Math.random()*90000) + 10000;
             return productId;
         })();
         this.name = name;
         this.price = price;
         this.expirationDate = expirationDate;
         this.getInfo = function() {
-            var a = this.name;
-            var code =  a[0] + a[a.length-1];
-            var codeUpperCasse = code.toUpperCase();
+            var nameOfProduct = this.name;
+            var code =  nameOfProduct[0] + nameOfProduct[nameOfProduct.length-1];
+            var codeUpperCasseLetters = code.toUpperCase();
 
-            return codeUpperCasse + this.id + ', ' + this.name + ', ' + this.price;
+            return codeUpperCasseLetters + this.id + ', ' + this.name + ', ' + this.price;
         }
     }
 
     function ShoppingBag() {
 
         this.list = [];
-
         
-        this.addProduct = function (p){
-            this.list.push(p);
+        this.addProduct = function (product){
+            var currentDate = new Date();
+            var expirationDate = new Date(2018, 9, 10);
+
+            if (expirationDate - currentDate >= 0) {
+                this.list.push(product);
+            }
         }
 
         this.getAverage = function() {
-            var p = 0;
-            var x;
+            var sum = 0;
+            var mean;
             for (var i = 0; i < this.list.length; i++) {
-                p += this.list[i].price;
+                sum += this.list[i].price;
             }
-            x = p / this.list.length;
+            mean = sum / this.list.length;
 
-            return x.toFixed(3);
+            return mean.toFixed(3);
         }
 
         this.getMostExpensive = function() {
             var max = this.list[0].price;
             for (var i = 1; i < this.list.length; i++) {
-                var product =  this.list[i] 
+                var product =  this.list[i];
+
                 if (max < product.price) {
                     max =product.price;
                 }
@@ -54,11 +55,12 @@
             return max;
         }
         this.calculateTotalPrice = function() {
-            var p = 0;
+            var sum = 0;
+
             for (var i = 0; i < this.list.length; i++) {
-                p += this.list[i].price;
+                sum += this.list[i].price;
             }
-            return p;
+            return sum;
         }
     }
 
@@ -68,8 +70,8 @@
         this.expirationDate = new Date(expirationDate);
     }
 
-    function checkoutAndBuy(foo, boo) {
-        if (foo.calculateTotalPrice() <= boo.accountBalance) {
+    function checkoutAndBuy(allItemsPrices, statusOnCard) {
+        if (allItemsPrices.calculateTotalPrice() <= statusOnCard.accountBalance) {
             return 'Successful';
         } else {
             return 'Not enough money';
@@ -82,12 +84,12 @@
     var coffee = new Product('Grand kafa', 102.45, coffeeExpirationDate);
     var milk = new Product('Moja kravica', 89.99, new Date(2018, 3, 21));
     var iceCream = new Product('Frikom', 250,44,new Date(2018, 4, 18));
-    var y = new ShoppingBag();
-    var l = new PaymentCard(480, true, '10.10.2020.');
-    y.addProduct(coffee);
-    y.addProduct(iceCream);
+    var totalProducts = new ShoppingBag();
+    var creditCard = new PaymentCard(480, true, '10.10.2020.');
+    totalProducts.addProduct(coffee);
+    totalProducts.addProduct(iceCream);
 
-    console.log(checkoutAndBuy(y, l));
+    console.log(checkoutAndBuy(totalProducts, creditCard));
 
 //     console.log(coffee.getInfo());
 //     console.log(milk.getInfo());
